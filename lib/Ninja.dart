d5401bbd277e54d408accb1b6ed245f656f25731
semozbest <keldsami@gmail.com>
2020-04-29 14:57:30 +0300

    added ninja

---
+++ lib/Ninja.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:NinjaCard(),
+));
+
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
