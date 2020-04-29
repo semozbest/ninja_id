d7d9a1ec2453d984ebfc97c29db23fdd78a6c50c
semozbest <keldsami@gmail.com>
2020-04-29 14:58:51 +0300

    Update Ninja.dart

--- lib/Ninja.dart
+++ lib/Ninja.dart
@@ -4,8 +4,3 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home:NinjaCard(),
 ));
-
-class NinjaCard extends StatefulWidget {
-  @override
-  _NinjaCardState createState() => _NinjaCardState();
-}
